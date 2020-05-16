ae346ab6c0d619207929d5e3301ce95b2665d7df
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-05-16 12:04:05 -0400

    Create Ichihime_and_Traingle.cpp

---
+++ Codeforces/Practice/Ichihime_and_Traingle.cpp
@@ -0,0 +1,34 @@
+/*
+Aldew
+PROB: "Ichihime and Triangle"
+CLASSIFICATION: Math
+
+Proving the traingle inequality:
+Since x < y and z = y
+
+x + z > y or x + y > y
+y + z > x
+y + z > y > x so y + z > x
+*/
+
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--){
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        cout << b << " " << c << " " << c << endl;
+
+    }
+    return 0;
+}
