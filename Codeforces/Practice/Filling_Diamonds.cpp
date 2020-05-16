ed373e3ffce60a81e4f03b1ab08444eaec881470
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-05-16 16:27:57 -0400

    Create Filling_Diamonds.cpp

---
+++ Codeforces/Practice/Filling_Diamonds.cpp
@@ -0,0 +1,29 @@
+/*
+"Filling Diamonds"
+Aldew
+Classification: math
+*/
+
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+There are n places to place a vertical diamond,
+the rest just fall around it.
+*/
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+
+        cout << n << endl;
+
+    }
+     return 0;
+}
