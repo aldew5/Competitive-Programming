d3455b2ece6901882f56e93eb1098a7dbef0b3fe
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-06-14 09:59:52 -0400

    Create Five_Variables.cpp

---
+++ AtCoder/Beginner Contest 170/Five_Variables.cpp
@@ -0,0 +1,33 @@
+/*
+Aldew
+PROB: Five Variables
+CLASSIFICATION: Implementation
+DIFFICULTY:
+*/
+ 
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <string>
+#include <map>
+#include <math.h>
+#include <cstdio>
+#define ll long long int
+ 
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+ 
+    if (a == 0) cout << 1 << endl;
+    else if (b == 0) cout << 2 << endl;
+    else if (c == 0) cout << 3 << endl;
+    else if (d == 0) cout << 4 << endl;
+    else cout << 5 << endl;
+    return 0;
+}
