bb771c3950b314d292a2d8a58432647c2912531b
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-06-19 08:48:15 -0400

    Create CountInversions.cpp

---
+++ Algorithms/CountInversions.cpp
@@ -0,0 +1,43 @@
+ll CountSplitInversions(int L[], int R[], int nl, int nr){
+
+    sort(L, L + nl);
+    sort(R, R + nr);
+
+    ll ans = 0;
+    int j = 0, i = 0;
+
+    while (i < nl && j < nr){
+        // inversion
+        if (L[i] > R[j]){
+            ans += nl - i;
+            j ++;
+        }
+        else {
+            i ++;
+        }
+    }
+    return ans;
+}
+
+ll CountInversions(int arr[], int n){
+
+    if (n == 1) return 0;
+    else {
+        int R[n-n/2], L[n/2];
+        for (int i = 0; i < n; i++){
+            if (i < n/2){
+                L[i] = arr[i];
+            }
+            else{
+                R[i - n/2] = arr[i];
+            }
+        }
+
+
+        ll x = CountInversions(L, n/2);
+        ll y = CountInversions(R, n - n/2);
+        ll z = CountSplitInversions(L, R, n/2, n- n/2);
+
+        return x + y + z;
+    }
+}
