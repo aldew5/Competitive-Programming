b41efa65cd72ceaafe28944d3c2760641df0071f
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-07-23 21:12:43 -0400

    Update A.cpp

--- Codeforces/EDU/Suffix Array/A.cpp
+++ Codeforces/EDU/Suffix Array/A.cpp
@@ -1,7 +1,8 @@
 /*
 ID: alec3
 LANG: C++14
-PROG: suffix array
+PROG: lamps
+credit to Russ Cox
 */
 
 
@@ -36,11 +37,17 @@ int main()
     vector<int> p(n), c(n);
 
     {
+        // a will contain the suffix array for k = 0
         vector<pair<char, int>> a(n);
         for (int i = 0; i < n; i++) a[i] = {s[i], i};
         sort(a.begin(), a.end());
 
+        // store the suffix array in p
         for (int i = 0; i < n; i++) p[i] = a[i].second;
+        // build the compare vector so we can compare
+        // strings in constant time over the next iteration
+        // each index of c corresponds to the same index in the
+        // sorted p vector which contains the suffix array
         c[p[0]] = 0;
         for (int i = 1; i < n; i++){
             if (a[i].first == a[i-1].first){
@@ -54,13 +61,18 @@ int main()
 
 
     int k = 0;
+    // loop through ks (powers of 2 that further sort the longer suffixes)
     while ((1 << k) < n){
         vector<pair<pair<int, int>, int>> a(n);
         for (int i = 0; i < n; i++){
+            // store the values for each have of the substring
+            // so we can sort them and their position
             a[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
         }
+        // sort and copy suffix array into c
         sort(a.begin(), a.end());
         for (int i = 0; i < n; i++) p[i] = a[i].second;
+        // build the new comparison vector
         c[p[0]] = 0;
         for (int i = 1; i < n; i++){
             if (a[i].first == a[i-1].first){
@@ -72,6 +84,7 @@ int main()
         }
         k ++;
     }
+    //output
     for (int i = 0; i < n; i++){
         cout << p[i] << " ";
     }
@@ -80,3 +93,4 @@ int main()
 
     return 0;
 }
+
