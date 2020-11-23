8512b94d7050908cc410772e87d4c6f295fc54cb
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-11-22 19:12:58 -0500

    Create Forest_Queries.cpp

---
+++ CSES/Forest_Queries.cpp
@@ -0,0 +1,86 @@
+/*
+ID: alec3
+LANG: C++14
+PROG:
+*/
+/*
+Credit to Benq for template
+*/
+
+
+#include <bits/stdc++.h>
+
+#define check(x) cout<<(#x)<<": "<<x<<" " << endl;
+#define line cout << "--------------------" << endl;
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ss second
+#define ff first
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int pct(int x) { return __builtin_popcount(x); }
+
+using namespace std;
+void setIO(string name) {
+	freopen((name+".in").c_str(),"r",stdin);
+	freopen((name+".out").c_str(),"w",stdout);
+	ios_base::sync_with_stdio(0);
+}
+
+template<class T, int SZ> struct PrefixSums {
+    T sum[SZ][SZ];
+    PrefixSums () { memset(sum,0,sizeof sum); }
+    // 2D prefix sum
+    void init() {
+        FOR(i,1,SZ) FOR(j,1,SZ)
+        sum[i][j] += sum[i][j-1]
+        +sum[i-1][j]-sum[i-1][j-1];
+    }
+    // get the sum for a given range
+    T get(int X1, int X2, int Y1, int Y2) {
+        return sum[X2][Y2]-sum[X1-1][Y2]
+        -sum[X2][Y1-1]+sum[X1-1][Y1-1];
+    }
+};
+
+PrefixSums<int, 1001> P;
+
+using namespace std;
+
+int grid[1001][1001];
+
+int main()
+{
+    //setIO("homework");
+    int n, q;
+    cin >> n >> q;
+
+    char it;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cin >> it;
+            if (it == '*')
+                P.sum[i+1][j+1] = 1;
+            else
+                P.sum[i+1][j+1] = 0;
+        }
+    }
+
+    P.init();
+    F0R(i, q){
+        int y1, x1, y2, x2;
+        cin >> y1 >> x1 >> y2 >> x2;
+        cout << P.get(y1, y2, x1, x2) << endl;
+    }
+
+    return 0;
+}
