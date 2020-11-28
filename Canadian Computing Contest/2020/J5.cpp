722fbfe4a17e81c1fd197c02a1156b80b955ba33
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2020-11-28 16:17:08 -0500

    Create J5.cpp

---
+++ Canadian Computing Contest/2020/J5.cpp
@@ -0,0 +1,117 @@
+/*
+ID: alec3
+LANG: C++14
+PROG:
+/*
+Credit to Michael Cao
+*/
+
+#include <bits/stdc++.h>
+
+#define check(x) cout<<(#x)<<": "<<x<<" " << endl;
+#define line cout << "--------------" << endl;
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ss second
+#define ff first
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define ld long double
+#define mp make_pair
+
+#define all(c) (c).begin(), (c).end()
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define F0R(i, n) for (int i = 0; i < n; i++)
+
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int pct(int x) { return __builtin_popcount(x); }
+
+using namespace std;
+
+void setIO(string name) {
+	freopen((name+".in").c_str(),"r",stdin);
+	freopen((name+".out").c_str(),"w",stdout);
+	ios_base::sync_with_stdio(0);
+}
+
+template<class T, int SZ> struct PrefixSums {
+    T sum[SZ][SZ];
+    PrefixSums () { memset(sum,0,sizeof sum); }
+    // 2D prefix sum
+    void init() {
+        FOR(i,1,SZ) FOR(j,1,SZ)
+        sum[i][j] += sum[i][j-1]
+        +sum[i-1][j]-sum[i-1][j-1];
+    }
+    // get the sum for a given range
+    T get(int X1, int X2, int Y1, int Y2) {
+        return sum[X2][Y2]-sum[X1-1][Y2]
+        -sum[X2][Y1-1]+sum[X1-1][Y1-1];
+    }
+};
+
+int n, m;
+ll grid[2002][2002];
+bool visited[2002][2002];
+
+vector<pair<ll, ll> > d[1000001];
+
+bool BFS(pair<ll, ll> src, pair<ll, ll> dest){
+    //cout << "CALLED " << endl;
+
+    queue<pair<ll,ll> > q;
+    q.push(src);
+
+
+    while (!q.empty()){
+        pair<ll, ll> node = q.front();
+        q.pop();
+       // cout << node.ff << " " << node.ss << endl;
+       // cout << grid[node.ff][node.ss] << endl;
+
+        if (node.ff == n && node.ss == m)
+            return true;
+
+        if (!visited[node.ff][node.ss]){
+            visited[node.ff][node.ss] = true;
+
+            ll val = grid[node.ff][node.ss];
+            for (int i = 0; i < d[val].size(); i++){
+
+                q.push(mp(d[val][i].ff, d[val][i].ss));
+
+            }
+        }
+    }
+    return false;
+
+}
+
+
+int main()
+{
+    //setIO("fenceplan");
+    io;
+    cin >> n;
+    cin >> m;
+    ll it;
+    for (int i= 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            cin >> it;
+            grid[i][j] = it;
+
+            d[i *j].pb(mp(i, j));
+        }
+    }
+
+    if (BFS({1,1}, {n, m}))
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+    return 0;
+
+}
