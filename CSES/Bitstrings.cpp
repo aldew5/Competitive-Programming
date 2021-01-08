8e4375a7df21b819392440ccaa606dbe68c6762c
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-01-08 00:15:30 -0500

    Create Bitstrings.cpp

---
+++ CSES/Bitstrings.cpp
@@ -0,0 +1,56 @@
+/*
+ID: alec3
+LANG: C++14
+PROG:
+*/
+/*
+*/
+
+
+#include <bits/stdc++.h>
+
+#define check(x) cout<<(#x)<<": "<<x<<" " << endl;
+#define line cout << "--------------------" << endl;
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ss second
+#define ff first
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int pct(int x) { return __builtin_popcount(x); }
+
+using namespace std;
+void setIO(string name) {
+	freopen((name+".in").c_str(),"r",stdin);
+	freopen((name+".out").c_str(),"w",stdout);
+	ios_base::sync_with_stdio(0);
+}
+
+
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    ll ans = 1;
+    for (int i = 0; i < n; i++){
+        ans *= 2;
+        ans %= MOD;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
