a871d0ef38527dc39d5be5c2797230b620ae4d9e
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-01-12 21:20:41 -0500

    Create Disjoint_Set_Union.h

---
+++ Algorithms and Data Structures/Disjoint_Set_Union.h
@@ -0,0 +1,15 @@
+struct DSU {
+    vector<int> e;
+    void init (int n) { e = vector<int> (n, -1); }
+    int get (int x) { return (e[x] < 0 ? x : e[x] = get(e[x])); }
+    bool sameSet (int x, int y) { return get(x) == get(y); }
+    int size (int x) { return -e[get(x)]; }
+    bool unite (int x, int y) {
+        x = get(x), y = get(y);
+        if (x == y) return 0;
+        if (e[x] > e[y]) swap(x, y);
+        e[x] += e[y];
+        e[y] = x;
+        return 1;
+    }
+};
