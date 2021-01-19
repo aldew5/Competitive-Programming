f9c4a0bd34a29580e2e8a30e4c6e7c6a2ecdf970
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-01-18 22:34:40 -0500

    Create Dijkstra.cpp

---
+++ Algorithms and Data Structures/Dijkstra.cpp
@@ -0,0 +1,26 @@
+// credit to CPI
+void dijkstra(int src){
+    for (int i = 0; i < n; i++)
+        dist[i] = LLONG_MAX;
+
+    using T = pair<ll,int>; priority_queue<T,vector<T>,greater<T>> pq;
+
+    dist[src] = 0;
+    pq.push({0, src});
+
+    while (pq.size()){
+        ll cdist, node;
+        tie(cdist, node) = pq.top();
+        pq.pop();
+
+        if (cdist != dist[node])
+            continue;
+        for (auto i : graph[node]){
+            // if we can reach a neighbouring node
+            // faster, we update its min distance
+            if (cdist + i.ss < dist[i.ff]){
+                pq.push({dist[i.ff] = cdist + i.ss, i.ff});
+            }
+        }
+    }
+}
