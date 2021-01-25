b8fd75c608e867ef84e486bd9a1cf5817cc4bb61
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-01-24 23:48:21 -0500

    Create Exponentiation.cpp

---
+++ CSES/Exponentiation.cpp
@@ -0,0 +1,66 @@
+
+/*
+ID: alec3
+LANG: C++14
+PROG:
+*/
+/*
+
+*/
+
+
+#include <bits/stdc++.h>
+
+#define check(x) cout<<(#x)<<": "<<x<<" " << endl;
+#define line cout << "--------------------" << endl;
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ss second
+#define ff first
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int pct(int x) { return __builtin_popcount(x); }
+
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e10 + 70;
+const ll MX = 2 * 1e6;
+
+ll binpow(ll x, ll n, ll m) {
+    assert(n >= 0);
+    x %= m; //note: m*m must be less than 2^63 to avoid ll overflow
+    ll res = 1;
+    while (n > 0) {
+        if (n % 2 == 1) //if n is odd
+            res = res * x % m;
+        x = x * x % m;
+        n /= 2; //divide by two
+    }
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    ll a, b;
+    F0R(i, n){
+        cin >> a >> b;
+        cout << binpow(a, b, MOD) << endl;
+    }
+
+
+
+    return 0;
+}
+
