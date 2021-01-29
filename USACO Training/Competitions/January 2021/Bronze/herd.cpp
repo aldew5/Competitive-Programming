e57956211b5402ae2043c53c61a23077e8dc25aa
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-01-29 00:16:28 -0500

    Create herd.cpp

---
+++ USACO Training/Competitions/January 2021/Bronze/herd.cpp
@@ -0,0 +1,57 @@
+/*
+ID: alec3
+LANG: C++14
+PROG:
+*/
+/*
+
+*/
+
+
+#include <bits/stdc++.h>
+
+#define check(x) cout<<(#x)<<": "<<x<<" " << endl;
+#define line cout << "--------------------" << endl;
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ss second
+#define ff first
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int pct(int x) { return __builtin_popcount(x); }
+
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e10 + 70;
+const ll MX = 2 * 1e6;
+
+
+int main()
+{
+    string alph, s;
+    cin >> alph >> s;
+
+    int ans = 0;
+    int index = 0;
+    while (index < s.size()){
+        for (int j = 0; j < alph.size(); j++){
+            if (alph[j] == s[index])
+                index ++;
+        }
+        ans ++;
+    }
+    cout << ans << endl;
+
+
+    return 0;
+}
