0ffdbd9e575037164fa0265daec52b8148a364b1
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-03-29 23:42:30 -0400

    Create Napolean_Cake.cpp

---
+++ Codeforces/Round  707/Napolean_Cake.cpp
@@ -0,0 +1,82 @@
+/*
+ID: alec3
+LANG: C++14
+PROG:
+*/
+/*
+
+*/
+
+
+#include <bits/stdc++.h>
+
+//#define check(x) cout<<(#x)<<": "<<x<<" " << endl;
+#define line cout << "--------------------" << endl;
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#define ss second
+#define ff first
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int pct(int x) { return __builtin_popcount(x); }
+
+using namespace std;
+
+const ll INF = 1e10 + 70;
+const int MOD = 1e9 + 7;
+const int MX = 2e5 + 5;
+
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0); // see /general/fast-io
+	if (name.size()) {
+		freopen((name+".in").c_str(), "r", stdin); // see /general/io
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (auto& it : a) cin >> it;
+        reverse(a.begin(), a.end());
+
+        int val = a[0];
+        vector<int> ans;
+
+        if (val)
+            ans.pb(1);
+        else
+            ans.pb(0);
+
+        val --;
+        for (int i = 1; i < n; i++){
+            val = max(val, a[i]);
+            if (val)
+                ans.pb(1);
+            else
+                ans.pb(0);
+            val --;
+        }
+        reverse(ans.begin(), ans.end());
+        for (auto i : ans)
+            cout << i << " ";
+        cout << endl;
+
+    }
+
+}
